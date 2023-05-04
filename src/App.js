938f91600fdc5ce63bcee975bad7b3c25ad26728
irgimondo <89301345+irgimondo@users.noreply.github.com>
2023-05-04 15:57:27 +0700

    create

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import './normal.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className='sidemenu'>
+        <div className='side-menu-button'>
+          <span>
+            +
+          </span>
+          New Chat
+        </div>
+      </aside>  
+      <section className='chatbox'>
+        <div className='chat-log'>
+          <div className='chat-message'>
+            <div 
+            className='chat-message-center'>
+              <div className='avatar'>
+
+              </div>
+              <div className='message'>
+                  Hello world
+              </div>
+            </div>
+            </div>
+            <div className='chat-message chatgpt'>
+            <div 
+            className='chat-message-center'>
+              <div className='avatar chatgpt'>
+
+              </div>
+              <div className='message'>
+                  I am villain
+              </div>
+
+            </div>
+            </div>
+        </div>
+        <div
+        className="chat-input-holder">
+          <textarea
+          rows="1"
+          className="chat-input-textarea"
+          placeholder = "Type Your message here">
+          </textarea>
+        </div>
+      </section>
     </div>
   );
 }
